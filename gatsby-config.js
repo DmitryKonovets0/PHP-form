2cf9656cebf93fa9b15dd0cdf676388dc14abc18
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-03-24 12:15:03 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Email request`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
